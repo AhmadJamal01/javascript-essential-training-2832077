0eafd471fce031740aaa7d863ed8a738bd0cf8a7
AhmadJamal01 <65499354+AhmadJamal01@users.noreply.github.com>
2021-08-12 13:13:56 +0200

    stuck on 5_10

--- 05_10/script.js
+++ 05_10/script.js
@@ -1,10 +1,10 @@
 /**
  * Challenge: Add a new element
  * - In JavaScript, create a new element to hold a navigation menu
- * - Add an unordered list and a series of no less than five links to the list 
+ * - Add an unordered list and a series of no less than five links to the list
  * - Use single words like “home”, “about”, etc for the list items and set the src attribute to # for simplicity
  * - Add the new navigation element to the DOM directly after the header
- * - Write basic CSS and add classes as necessary to create a horizontal layout for the menu. 
+ * - Write basic CSS and add classes as necessary to create a horizontal layout for the menu.
  * - A tip: Use either display flex or display grid to create the horizontal menu.
  */
 
@@ -60,3 +60,29 @@ newArticle.setAttribute("id", "everyday");
 newArticle.innerHTML = content;
 
 main.append(newArticle);
+
+const navContent = `
+  <ul style="list-style-type: none;">
+    <li><a href="" class="join">Home</a></li>
+    <li><a href="" class="about">About</a></li>
+    <li><a href="" class="new">New Backpacks</a></li>
+    <li><a href="" class="create">Create your Own</a></li>
+    <li><a href="" class="contact">Contact</a></li>
+  </ul>
+`;
+const navBar = document.createElement("nav");
+navBar.classList.add("main-nav");
+navBar.setAttribute(
+  "style",
+  "position:relative; display: grid; grid-template-columns: repeat(5,1fr);grid-auto-rows: minmax(100px,auto);margin: 0 auto;max-width: 100%"
+);
+const navLi = document.querySelectorAll(".main-nav li");
+/*navLi.forEach((li) => {
+  //li.style.width = "24%";
+  li.style.display = "inline-block";
+});*/
+navBar.innerHTML = navContent;
+//const parentDiv=document.querySelector(".siteheader").parentNode;
+//const childNode=document.querySelector(".siteheader");
+const headerElement = document.querySelector(".siteheader");
+headerElement.insertAdjacentElement("afterend", navBar);
--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,12 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+ document.querySelector("main li:first-child").classList.add("");//replace, toggle, remove
+ document.querySelector("").attributes //hasAttribute,getAttribute,setAttribute,
+ document.querySelector("").style.color //cssText
+
+ document.querySelector("li.feature.backpack__strap").getAttribute("data-side");
+ document.querySelector("li.feature.backpack__strap").classList.add("leftStrap","leftS");
+ document.querySelector("li.leftStrap").style.backgroundColor="blue"
+
