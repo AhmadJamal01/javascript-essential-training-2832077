1091c9462b5b9fb89a52c3d4a65cfbd2d544dae2
AhmadJamal01 <65499354+AhmadJamal01@users.noreply.github.com>
2021-08-17 03:40:56 +0200

    progress

--- 07_04/script.js
+++ 07_04/script.js
@@ -2,10 +2,30 @@
  * Challenge: Build and modify an array
  * - Build an array with 8 items
  * - Remove the last item
- * - Add the last item as the first item on the array 
+ * - Add the last item as the first item on the array
  * - Sort the items by alphabetical order
  * - Use the find() method to find a specific item in the array
  * - Remove the item you found using the find method from the array.
  */
 
-
+const items = [
+  "laptop",
+  "towel",
+  "charger",
+  5,
+  "perfume",
+  "pills",
+  "alcohol",
+  "visa",
+];
+console.log(items);
+const lastItem = items.pop();
+console.log(lastItem);
+console.log(items);
+items.unshift(lastItem);
+console.log(items);
+items.sort();
+console.log(items);
+const removed = items.splice(items.indexOf("alcohol"), 1);
+console.log(removed);
+console.log(items);
--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,82 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+/*
+const newArt = createArticle();
+
+function addArticle(article) {
+  const newArticle = document.createElement("article");
+  newArticle.innerHTML = article;
+  const main = document.querySelector("main");
+  main.append(newArticle);
+  console.log("Article added!");
+}
+
+function createArticle() {
+  const article = `
+    <h1>MAKING AN ARTICLE</h1>
+    <ul>
+        <li>item1</li>
+        <li>item2</li>
+        <li>item3</li>
+        <li>item4</li>
+    </ul>
+    `;
+  return article;
+}
+
+addArticle(newArt);
+*/
+///////////////////////////////////////////////////////////////////////////////////////
+/*
+const addArt = function (article) {
+  const newArticle = document.createElement("article");
+  newArticle.innerHTML = article;
+  const main = document.querySelector("main");
+  main.append(newArticle);
+  console.log("Article added!");
+};
+
+const newArt2 = function () {
+    const article = `
+    <h1>MAKING AN ARTICLE</h1>
+    <ul>
+    <li>item1</li>
+    <li>item2</li>
+    <li>item3</li>
+    <li>item4</li>
+    </ul>
+    `;
+    return article;
+};
+
+const art2 = newArt2();
+addArt(art2);
+*/
+
+/////////////////////////////////////////////////////////////////
+
+const addArt3 = (article) => {
+  const newArticle = document.createElement("article");
+  newArticle.innerHTML = article;
+  const main = document.querySelector("main");
+  main.append(newArticle);
+  console.log("Article added!");
+};
+
+const newArt3 = () => {
+  const article = `
+    <h1>MAKING AN ARTICLE</h1>
+    <ul>
+    <li>item1</li>
+    <li>item2</li>
+      <li>item3</li>
+      <li>item4</li>
+      </ul>
+      `;
+  return article;
+};
+
+const art3 = newArt3();
+addArt3(art3);
